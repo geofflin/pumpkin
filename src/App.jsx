fcc82edf33f865d55f0950cf4806b5babf905684
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-10-25 15:08:39 -0400

    Added src folder with initial index and App jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import MainContainer from './containers/MainContainer.jsx';
+
+
+const App = () => (
+//   <Provider store={store}>
+//     <MainContainer />
+//   </Provider>
+  <div>
+    This is my Pumpkin App
+  </div>
+);
+
+export default App;
