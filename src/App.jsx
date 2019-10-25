81be87b4850538cb31f90275d993a85bf6c2a3ab
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-10-25 15:18:23 -0400

    Added App.jsx root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
-import MainContainer from './containers/MainContainer.jsx';
-
 
 const App = () => (
-//   <Provider store={store}>
-//     <MainContainer />
-//   </Provider>
   <div>
     This is my Pumpkin App
   </div>
