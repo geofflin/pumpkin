e25b5fc22b8e837fafc4bbca5c7ed95b58ef2868
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-10-25 17:29:39 -0400

    Added new routes for skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import {
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
+import MyPets from './components/MyPets';
 
 const App = () => (
-  <div>
-    This is my Pumpkin App
-  </div>
+  <Fragment>
+    <div><Link to="/">Home</Link></div>
+    <div><Link to="/my-pets">My Pets</Link></div>
+    <hr />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/my-pets" component={MyPets} />
+      <Route path="/pets/new" component={MyPets} />
+      <Route path="/pets/success" component={MyPets} />
+    </Switch>
+  </Fragment>
 );
 
 export default App;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
