8491ebb2905d923f1f65720f92341c1eb687cf4c
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-10-25 17:06:05 -0400

    Wrapped app with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 
-render(<App />, document.querySelector('#root'));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.querySelector('#root')
+);
