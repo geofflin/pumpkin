67d76101b3e4d6ecdbf9efa212e53dc3c265449c
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-10-26 11:44:44 -0400

    Set up jest boilerplate

---
+++ jest.config.js
@@ -0,0 +1,41 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // A list of paths to directories that Jest should use to search for files in
+  "roots": [
+    "<rootDir>/__tests__"
+  ],
+
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // A path to a module which exports an async function that is triggered once before all test suites
+  // globalSetup: null,
+
+  // A path to a module which exports an async function that is triggered once after all test suites
+  // globalTeardown: null,
+
+  // An array of file extensions your modules use
+  // moduleFileExtensions: [
+  //   "js",
+  //   "json",
+  //   "jsx",
+  //   "ts",
+  //   "tsx",
+  //   "node"
+  // ],
+
+  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
+  // snapshotSerializers: [],
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+
+};
